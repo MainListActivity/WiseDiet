bf68984c5921a37341b9febc393cadbbe43494dd
MainListActivity <1025988443@qq.com>
2026-01-30 21:58:14 +0800

    test: configure redis testcontainer

--- server/src/test/java/cn/cuckoox/wisediet/AbstractIntegrationTest.java
+++ server/src/test/java/cn/cuckoox/wisediet/AbstractIntegrationTest.java
@@ -9,13 +9,17 @@ import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
+import com.redis.testcontainers.RedisContainer;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers
 public abstract class AbstractIntegrationTest {
 
     @Container
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(DockerImageName.parse("postgres:15-alpine"));
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(DockerImageName.parse("postgres:16-alpine"));
+
+    @Container
+    static RedisContainer redis = new RedisContainer(DockerImageName.parse("redis:7.4-alpine"));
 
     @DynamicPropertySource
     static void configureProperties(DynamicPropertyRegistry registry) {
@@ -25,5 +29,7 @@ public abstract class AbstractIntegrationTest {
                 postgres.getDatabaseName()));
         registry.add("spring.r2dbc.username", postgres::getUsername);
         registry.add("spring.r2dbc.password", postgres::getPassword);
+        registry.add("spring.data.redis.host", redis::getHost);
+        registry.add("spring.data.redis.port", redis::getFirstMappedPort);
     }
 }
